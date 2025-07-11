2fc357dfad9261b275663ba5e1c5e0ba2b38f636
ViCoder <145913485+resources3312@users.noreply.github.com>
2025-07-11 15:38:48 +0300

    Add main function

--- nce.py
+++ nce.py
@@ -8,19 +8,20 @@ from sys import argv
 def command_list(filename: str) -> list[str]:
     with open(filename, "r", encoding="utf-8") as file: return file.read().splitlines()
 
-try:
-    system("clear")
-    [print(f"{i+1}. {c}") for i, c in enumerate(command_list(argv[1]))]
-    while True:
-        num = int(input("Enter option number: "))
-        if num == 0: break
-        else:
-            print(run(command_list(argv[1])[num - 1], shell=True))
-            input()
-            system("clear")
-            [print(f"{i+1}. {c}") for i, c in enumerate(command_list(argv[1]))]
+def main() -> None:
+    try:
+        system("clear")
+        [print(f"{i+1}. {c}") for i, c in enumerate(command_list(argv[1]))]
+        while True:
+            num = int(input("Enter option number: "))
+            if num == 0: break
+            else:
+                print(run(command_list(argv[1])[num - 1], shell=True))
+                input()
+                system("clear")
+                [print(f"{i+1}. {c}") for i, c in enumerate(command_list(argv[1]))]
 
+    except (IndexError, ValueError): pass
+    except KeyboardInterrupt: exit()
 
-except (IndexError, ValueError): pass
-except KeyboardInterrupt: exit()
-
+if __name__ == '__main__': main()
